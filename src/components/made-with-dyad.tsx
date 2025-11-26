270d1b4bfd0d0fa6fcaaa23443e375bd70ee03ad
[dyad] <git@dyad.sh>
2025-11-26 19:58:55 -0300

    [dyad] wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,1 +1,15 @@
--
+"use client";
+
+import React from 'react';
+
+export const MadeWithDyad = () => {
+  return (
+    <div className="p-4 text-center">
+      <span
+        className="text-sm text-gray-500 dark:text-gray-400"
+      >
+        Echo por EUGA - 2025
+      </span>
+    </div>
+  );
+};
