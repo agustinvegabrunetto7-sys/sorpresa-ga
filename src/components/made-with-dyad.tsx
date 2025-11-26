c843c975621cf25173008c4ecbc338e0d2766f59
[dyad] <git@dyad.sh>
2025-11-26 19:50:40 -0300

    Updated src/components/made-with-dyad.tsx

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,11 +1,1 @@
-export const MadeWithDyad = () => {
-  return (
-    <div className="p-4 text-center">
-      <span
-        className="text-sm text-gray-500 dark:text-gray-400"
-      >
-        Echo por EUGA - 2025
-      </span>
-    </div>
-  );
-};
+export const
